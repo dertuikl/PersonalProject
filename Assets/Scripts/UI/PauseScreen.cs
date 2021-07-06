fea54f84f1176a178e427690b4839d8e7bdab17e
Aleksandra <dertuikl@gmail.com>
2021-07-06 20:48:08 +0300

    Icons replaced with assets

--- Assets/Scripts/UI/PauseScreen.cs
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseScreen : MonoBehaviour
 {
-    public void OnShowStatsOnGameScreenToggle(bool value) => UserData.ShowShootingStatsToggle();
+    [SerializeField] private Sprite checkToggle;
+    [SerializeField] private Sprite uncheckToggle;
+    [SerializeField] private Image toggleImage;
+
+    public void OnShowStatsOnGameScreenToggle(bool value)
+    {
+        UserData.ShowShootingStatsToggle();
+        toggleImage.sprite = UserData.ShowShootingStats ? checkToggle : uncheckToggle;
+    }
 
     public void OnClickMenu()
     {
