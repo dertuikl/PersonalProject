91f2106c1d594d26930d5a1b7227ceddc40f70b1
Aleksandra <dertuikl@gmail.com>
2021-07-08 01:09:38 +0300

    Enemy refactoring. Added nav mesh agent

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,14 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public Action<Enemy> OnKill;
 
+    [SerializeField] private bool useAgentToMove = true;
+
     [SerializeField] private float moveSpeed;
     [SerializeField] private int maxHealth = 100;
     [SerializeField] private int killScore = 1;
+
+    [SerializeField] private NavMeshAgent agent;
 
     [SerializeField] private Transform body;
     [SerializeField] private Slider healthSlider;
@@ -19,30 +24,45 @@ public class Enemy : MonoBehaviour
     private float currentHealth;
 
     public int KillScore => killScore;
-    private PlayerController player => GameController.Instance.Player;
+    private PlayerController player;// => GameController.Instance.Player;
 
     private void OnEnable()
     {
         currentHealth = maxHealth;
         healthSlider.maxValue = maxHealth;
+
+        player = GameController.Instance.Player;
+
+        AgentSetup();
         UpdateHealthSlider();
+    }
+
+    private void AgentSetup()
+    {
+        agent.speed = moveSpeed;
     }
 
     private void Update()
     {
         if (player && GameController.Instance.GameIsActive) {
             MoveToPlayer();
+        } else {
+            agent.isStopped = true;
         }
     }
 
     private void MoveToPlayer()
     {
-        Vector3 moveVector = player.transform.position - transform.position;
-        // TODO: how track bullet hit in that case?
-        //collider.enabled = moveVector.magnitude < collider.size.x;
+        if (useAgentToMove) {
+            agent.SetDestination(player.transform.position);
+        } else {
+            Vector3 moveVector = player.transform.position - transform.position;
+            // TODO: how track bullet hit in that case?
+            //collider.enabled = moveVector.magnitude < collider.size.x;
 
-        Vector3 moveDirection = moveVector.normalized;
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+            Vector3 moveDirection = moveVector.normalized;
+            transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+        }
 
         body.LookAt(player.transform);
     }
